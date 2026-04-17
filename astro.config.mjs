9d01a3d98fdedb84dc7e0b8c2532487871734f0f
Cedrick <info@cliniqueduvr.ca>
2026-04-17 08:40:03 -0400

    Fix Astro site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www.cliniqueduvr.ca/',
+  site: 'https://demosolution.ca/',
   integrations: [],
   output: 'static',
   vite: {
